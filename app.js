51173bbc2f2bea7f4d5ec45435c0bac9c2f33aee
vdinojkumar <dkvd360@gmail.com>
2023-02-26 18:38:21 +0530

    exprss notes updated

--- app.js
+++ app.js
@@ -1,47 +1,26 @@
-const http=require('http')
-const {readFileSync}= require('fs')
+const express= require('express')
+const path= require('path')
 
+const app= express()
 
-//all files
-const homePage= readFileSync('./index.html')
-const homeStyles= readFileSync('./styles.css')
-const homeImage= readFileSync('./download.png')
-const homeLogic= readFileSync('./browser-app.js')
+//set up static middleware
+app.use(express.static('./public'))
 
-const server= http.createServer((req,res)=>{
-   
-    const url=req.url
+// app.get('/',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'./navbar/index.html'))
+//     adding to static assets
+//     server side rendering SSR
+// })
 
-    //home page
-    if(url==='/'){
-        res.writeHead(200,{'content-type':'text/html'})
-        res.write(homePage)
-        res.end()
-        
-    }
-
-    //styles
-    else if(url==='/styles.css'){
-        res.writeHead(200,{'content-type':'text/css'})
-        res.write(homeStyles)
-        res.end()
-    }
-
-    //logo
-    else if(url==='/download.png'){
-        res.writeHead(200,{'content-type':'image/png'})
-        res.write(homeImage)
-        res.end()
-    }
-    // java script
-    else if('/browser-app.js'){
-        res.writeHead(200,{'content-type':'text/javascript'})
-        res.write(homeLogic)
-        res.end()
-    }
-    
-    console.log(url);
-
+app.all('*',(req,res)=>{
+    res.status(404).send('<h1>resources are not found</h1>')
 })
 
-server.listen(5000)
+app.listen(5000,()=>{
+    console.log('server is listning');
+})
+
+
+// app.get('/about',(req,res)=>{
+//     res.send('about pages')
+// })
