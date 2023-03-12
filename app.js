77440db696436f6167911c50369ba4f528c19e63
vdinojkumar <dkvd360@gmail.com>
2023-03-12 20:59:15 +0530

    setting up using multiple middleware in nodejs

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
 const express=require('express')
 const app=express()
+const logger=require('./logger')
+const authorize=require('./authorize')
+
+
+//middleware function
+//path specifies , middleware to invoke when it hits that path
+// ****app.use('/api',logger)
+// it is applied to all the req 
+// to use mutiple middleware we use array 
+app.use([logger,authorize])
+
+
+app.get('/',(req,res)=>{
+    res.send('home')
+})
+
+app.get('/about',(req,res)=>{
+    res.send('about')
+})
+
+app.get('/api/product',(req,res)=>{
+    res.send('product')
+})
+
+app.get('/api/desc',(req,res)=>{
+    res.send('about')
+})
+
 
 
 app.listen(5000,()=>{
---
+++ authorize.js
@@ -0,0 +1,6 @@
+const authorize=(req,res,next)=>{
+    console.log("auhtorize")
+    next()
+}
+
+module.exports=authorize
